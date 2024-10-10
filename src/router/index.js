f4ccb33f46f69112bb88eb28fef2479ea4b38dca
Adel Amrane <Adelgamer814@gmail.com>
2024-10-10 16:03:39 +0100

    Fixed indentations

--- src/router/index.js
+++ src/router/index.js
@@ -6,35 +6,35 @@ import SettingsPage from "@/views/SettingsPage.vue";
 import AboutPage from "@/views/AboutPage.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "current",
-    component: CurrentPage,
-  },
-  {
-    path: "/hourly",
-    name: "hourly",
-    component: HourlyPage,
-  },
-  {
-    path: "/daily",
-    name: "daily",
-    component: DailyPage,
-  },
-  {
-    path: "/settings",
-    name: "settings",
-    component: SettingsPage,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: AboutPage,
-  },
+    {
+        path: "/",
+        name: "current",
+        component: CurrentPage,
+    },
+    {
+        path: "/hourly",
+        name: "hourly",
+        component: HourlyPage,
+    },
+    {
+        path: "/daily",
+        name: "daily",
+        component: DailyPage,
+    },
+    {
+        path: "/settings",
+        name: "settings",
+        component: SettingsPage,
+    },
+    {
+        path: "/about",
+        name: "about",
+        component: AboutPage,
+    },
 ];
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
 });
 
 export default router;
